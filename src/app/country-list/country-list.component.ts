aab59dd8d4d3e24eeaa3e000210de029bd6de42a
Aina Shokota <alina.shokota159@gmail.com>
2019-04-14 13:23:41 +0200

    lista

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Country} from '../shared/model/country';
 
 @Component({
   selector: 'app-country-list',
@@ -9,7 +10,14 @@ export class CountryListComponent implements OnInit {
 
   constructor() { }
 
+  countries: Country[] = new Array();
   ngOnInit() {
+    const country: Country = new Country();
+    country.id = 1;
+    country.name = 'Poland';
+    country.continentId = 70;
+    this.countries[0] = country;
+    console.log(this.countries);
   }
 
 }
